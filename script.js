14b95a2fe6593dc6f724c3bf034170807a5d4584
sodabarauf <sodabarauf4@gmail.com>
2024-06-22 17:57:08 +0430

    added new files

---
+++ script.js
@@ -0,0 +1,29 @@
+let textInput = document.getElementById("text-input");
+let checkBtn = document.getElementById("check-btn");
+let result = document.getElementById("result");
+
+checkBtn.addEventListener("click", palindrome);
+
+function palindrome() {
+  if (textInput.value == ""){
+    alert("please input a value");
+    result.innerHTML = "";
+    return;
+  }
+
+  const processedStr = textInput.value.replace(/[\W_]/g, "").toLowerCase();
+  let ispalindrome = true;
+
+  for (let i = 0; i < Math.floor(processedStr.length / 2); i++) {
+    if (processedStr[i] !== processedStr[processedStr.length - 1 - i]) {
+      ispalindrome = false;
+      break;
+    }
+  }
+
+  if (ispalindrome) {
+    result.innerHTML = textInput.value + " is a palindrome";
+  } else {
+    result.innerHTML = textInput.value + " is not a palindrome";
+  }
+}
