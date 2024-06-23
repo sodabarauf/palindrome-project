ad7ecf00d0656f8ad3fe761e6f6eec9a2ab5826d
sodabarauf <sodabarauf4@gmail.com>
2024-06-23 19:55:00 +0430

    fixed linters error

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const textInput = document.getElementById('text-input');
 const checkBtn = document.getElementById('check-btn');
 const result = document.getElementById('result');
 
-checkBtn.addEventListener('click', palindrome);
 
 function palindrome() {
   if (textInput.value === '') {
@@ -11,10 +10,12 @@ function palindrome() {
     return;
   }
 
+  checkBtn.addEventListener('click', palindrome);
+
   const processedStr = textInput.value.replace(/[\W_]/g, '').toLowerCase();
   let ispalindrome = true;
 
-  for (let i = 0; i < Math.floor(processedStr.length / 2); i++) {
+  for (let i = 0; i < Math.floor(processedStr.length / 2); i) {
     if (processedStr[i] !== processedStr[processedStr.length - 1 - i]) {
       ispalindrome = false;
       break;
@@ -22,8 +23,8 @@ function palindrome() {
   }
 
   if (ispalindrome) {
-    result.innerHTML = textInput.value + ' is a palindrome';
+    result.innerHTML = textInput.value + " is a palindrome";
   } else {
-    result.innerHTML = textInput.value + ' is not a palindrome';
+    result.innerHTML = textInput.value + " is not a palindrome";
   }
 }
